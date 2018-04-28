6a5444cf8216c97c0f5ce3e396e5706ab93bce28
kanoonsantikul <niti.sa@ku.th>
2018-04-28 23:02:56 +0700

    merge camera and image page

--- scripts/camera.js
+++ scripts/camera.js
@@ -1,74 +1,96 @@
-var pictures = []
+'use strict'
 
-var videoObj    = { "video": true },
-    errBack        = function(error){
-        // alert("Video capture error: ", error.code);
-    };
+var images = [];
+var video, image;
+var buttonTake, buttonRetake, buttonProceed;
 
-// Ask the browser for permission to use the Webcam
-if(navigator.getUserMedia){                    // Standard
+function loadCamera () {
+  video = createTag('video');
+  video.id = 'video'
+  video.width = video.offsetWidth;
+
+  image = createTag('img');
+  image.id = 'image';
+  image.style.display = 'none';
+
+  var row = createTag('div', 'row');
+
+  buttonTake = createTag('button', '', row);
+  buttonTake.id = 'button-take';
+  buttonTake.innerHTML = 'Take';
+
+  buttonRetake = createTag('button', '', row);
+  buttonRetake.id = 'button-retake';
+  buttonRetake.innerHTML = 'Retry';
+  buttonRetake.style.display = 'none';
+
+  row = createTag('div', 'row');
+  buttonProceed = createTag('a', 'btn btn-outline-primary btn-sm');
+  buttonProceed.id = 'button-proceed'
+  buttonProceed.innerHTML = 'Proceed';
+
+  askPermission();
+}
+
+function askPermission () {
+  var videoObj = {
+    "video": true,
+    "deviceId": {exact: 2}
+  };
+  var errBack = function(error){
+    alert("Video capture error: ", error.code);
+  };
+
+  if(navigator.getUserMedia) {                      // Standard
     navigator.mediaDevices.getUserMedia(videoObj).then(startWebcam);
-}else if(navigator.webkitGetUserMedia){        // WebKit
+  } else if (navigator.webkitGetUserMedia) {        // WebKit
     navigator.webkitGetUserMedia(videoObj, startWebcam, errBack);
-}else if(navigator.mozGetUserMedia){        // Firefox
+  }else if (navigator.mozGetUserMedia) {            // Firefox
     navigator.mozGetUserMedia(videoObj, startWebcam, errBack);
-};
+  };
+}
 
-function startWebcam(stream){
-    var my_online_camera    = document.getElementById("my_online_camera");
-    var video            = document.getElementById("video");
-    var canvas            = document.getElementById("canvas");
-
-    video.width = video.offsetWidth;
-
-    if(navigator.mediaDevices.getUserMedia){                    // Standard
-        video.srcObject = stream;
-        video.onloadedmetadata = function(e) {
-            video.play();
-        };
-    }else if(navigator.webkitGetUserMedia){        // WebKit
-        video.src = window.webkitURL.createObjectURL(stream);
-        video.play();
-    }else if(navigator.mozGetUserMedia){        // Firefox
-        video.src = window.URL.createObjectURL(stream);
-        video.play();
+function startWebcam (stream) {
+  if(navigator.mediaDevices.getUserMedia){      // Standard
+    video.srcObject = stream;
+    video.onloadedmetadata = function(e) {
+      video.play();
     };
+  }else if(navigator.webkitGetUserMedia){       // WebKit
+    video.src = window.webkitURL.createObjectURL(stream);
+    video.play();
+  }else if(navigator.mozGetUserMedia){          // Firefox
+    video.src = window.URL.createObjectURL(stream);
+    video.play();
+  };
 
-    //Click to take the photo
-    $("#take_picture").click(function(){
-        // Copying the image in a temporary canvas
-        canvas.style.display = "block";
-        var temp = document.createElement("canvas");
+  $("#button-take").click(function(){
+    var canvas = document.createElement("canvas");
+    canvas.width = video.offsetWidth;
+    canvas.height = video.offsetHeight;
+    canvas.getContext('2d')
+          .drawImage(video, 0, 0, canvas.width, canvas.height);
 
-        temp.width  = video.offsetWidth;
-        temp.height = video.offsetHeight;
+    image.src = canvas.toDataURL();
+    images.push(image.src)
 
-        var tempcontext = temp.getContext("2d"),
-            tempScale = (temp.height/temp.width);
+    image.style.display = "block";
+    video.style.display = "none";
 
-        tempcontext.drawImage(
-            video,
-            0, 0,
-            video.offsetWidth, video.offsetHeight
-        );
+    buttonTake.style.display = 'none';
+    buttonRetake.style.display = 'block';
+  });
 
-        // Resize it to the size of our canvas
-        canvas.style.height    = parseInt( canvas.offsetWidth * tempScale );
-        canvas.width        = canvas.offsetWidth; 
-        canvas.height        = canvas.offsetHeight;
-        var context        = canvas.getContext("2d"),
-            scale        = canvas.width/temp.width;
-        context.scale(scale, scale);
-        context.drawImage(video, 0, 0, canvas.width, canvas.height);
+  $("#button-retake").click(function(){
+    video.style.display = "block";
+    image.style.display = "none";
 
-        camera = document.getElementById("my_online_camera")
-        camera.style.display = "none";
+    buttonTake.style.display = 'block';
+    buttonRetake.style.display = 'none';
+  })
 
-    });
-
-    $("#re_take_picture").click(function(){
-        camera.style.display = "block";
-        canvas.style.display = "none";
-    })
-};
-
+  $("#button-proceed").click(function(){
+    clear();
+    loadPictureSummary();
+  })
+}
