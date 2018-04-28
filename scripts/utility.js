7045baa0a6d0100039634fe2ca07456ba37a79f7
kanoonsantikul <niti.sa@ku.th>
2018-04-28 20:46:30 +0700

    add image summary page

--- scripts/utility.js
+++ scripts/utility.js
@@ -1,6 +1,23 @@
+'use strict'
+
+const CONTAINER = document.getElementById('main-container');
+
 function clear () {
-  contains = document.getElementsById('main-container');
+  contains = document.getElementById('main-container');
   while (contains.firstChild) {
     contains.removeChild(contains.firstChild);
   }
+}
+
+function createTag (tagName, className = '', parent = undefined) {
+  var tag = document.createElement(tagName);
+  tag.className = className;
+
+  if (parent) {
+    parent.appendChild(tag);
+  } else {
+    CONTAINER.append(tag);
+  }
+  
+  return tag;
 }
